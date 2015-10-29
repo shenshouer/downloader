67a573437baa79f1a17e9e77c704706bf75b8ee1
sope <shenshouer51@gmail.com>
2015-10-29 18:57:06 +0800

    -

--- ftp4goTest.go
+++ ftp4goTest.go
@@ -2,29 +2,31 @@ package main
 
 import (
 	ftp4go "github.com/shenshouer/ftp4go"
-	"fmt"
 	"os"
+	"log"
 )
 
 var(
-	downloadFileName 	= "DockerToolbox-1.8.2a.pkg"
+	downloadFileName 	= "DockerToolbox-1.8.2a.pkg1"
 	BASE_FTP_PATH 		= "/home/bob/"					// base data path in ftp server
 )
 
 func main() {
+	log.SetFlags(log.Flags()|log.Lshortfile)
+
 	ftpClient := ftp4go.NewFTP(0) // 1 for debugging
 
 	//connect
 	_, err := ftpClient.Connect("172.8.4.101", ftp4go.DefaultFtpPort, "")
 	if err != nil {
-		fmt.Println("The connection failed")
+		log.Println("The connection failed. err:", err)
 		os.Exit(1)
 	}
 	defer ftpClient.Quit()
 
 	_, err = ftpClient.Login("bob", "p@ssw0rd", "")
 	if err != nil {
-		fmt.Println("The login failed")
+		log.Println("The login failed err:", err)
 		os.Exit(1)
 	}
 
@@ -32,19 +34,19 @@ func main() {
 	var cwd string
 	cwd, err = ftpClient.Pwd()
 	if err != nil {
-		fmt.Println("The Pwd command failed")
+		log.Println("The Pwd command failed. err:",err)
 		os.Exit(1)
 	}
-	fmt.Println("The current folder is", cwd)
+	log.Println("The current folder is", cwd)
 
 
 	// get the remote file size
 	size, err := ftpClient.Size("/home/bob/"+downloadFileName)
 	if err != nil {
-		fmt.Println("The Pwd command failed")
-		os.Exit(1)
+		log.Println("The Size command failed. err:", err)
+		//os.Exit(1)
 	}
-	fmt.Println("size ", size)
+	log.Println("size ", size)
 
 	// start resume file download
 	if err = ftpClient.DownloadResumeFile("/home/bob/"+downloadFileName, "/Users/goyoo/ftptest/"+downloadFileName, false); err != nil{

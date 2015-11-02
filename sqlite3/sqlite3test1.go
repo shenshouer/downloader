0a6f9eb768c9e95552e5de0f965d70fa7d385eeb
sope <shenshouer51@gmail.com>
2015-11-02 19:24:12 +0800

    reflect to set struct value

--- sqlite3/sqlite3test1.go
+++ sqlite3/sqlite3test1.go
@@ -6,13 +6,15 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 	"log"
 	"time"
-	"github.com/fatih/structs"
+	"github.com/shenshouer/structs"
 	"strings"
+	"reflect"
 )
 
 type(
 	IEntity interface{
 		TableName() string
+		Initialize() IEntity
 	}
 
 	UserInfo struct {
@@ -25,6 +27,8 @@ type(
 
 var(
 	db *sql.DB
+	typeRegistry = make(map[string]reflect.Type)	// 用于orm查询时返回对应的struct实体
+
 	sql_create_userinfo = `
 	CREATE TABLE IF NOT EXISTS userinfo (
 		uid INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -37,6 +41,7 @@ var(
 func init() {
 	log.SetFlags(log.Flags()|log.Lshortfile)
 
+	Registry(new(UserInfo))
 	var err error
 	db, err = sql.Open("sqlite3", "./sqlite3test1.db")
 	if err != nil{
@@ -53,70 +58,60 @@ func init() {
 
 func main() {
 	// insert
-	ui := &UserInfo{
-		Username: "astaxie",
-		Department: "研发部门",
-		Created: time.Now(),
-	}
-	res, err := Save(db, ui)
-	if err != nil{
-		log.Fatal(err)
-	}
-
-	id, err := res.LastInsertId()
-	if err != nil{
-		log.Fatal(err)
-	}
-
-	fmt.Println(id)
+//	ui := &UserInfo{
+//		Username: "astaxie",
+//		Department: "研发部门",
+//		Created: time.Now(),
+//	}
+//	res, err := Save(db, ui)
+//	if err != nil{
+//		log.Fatal(err)
+//	}
+//
+//	id, err := res.LastInsertId()
+//	if err != nil{
+//		log.Fatal(err)
+//	}
+//
+//	fmt.Println(id)
 	// query
-	rows, err := db.Query("SELECT * FROM userinfo")
-	if err != nil{
-		log.Fatal(err)
-	}
+	entities, err := Query(db, new(UserInfo))
+	log.Println(err)
+	log.Println(entities)
 
-	for rows.Next() {
-		ui := &UserInfo{}
-		err = rows.Scan(&ui.Uid, &ui.Username, &ui.Department, &ui.Created)
-		if err != nil{
-			log.Fatal(err)
-		}
-		log.Println(ui)
-	}
-
-	ui = &UserInfo{
-		Uid: id,
-		Username: "shenshouer",
-		Department: "GT-基础部门",
-	}
-
-	// update
-	_, err = Update(db, ui)
-	if err != nil{
-		log.Fatal(err)
-	}
-
+//	ui = &UserInfo{
+//		Uid: id,
+//		Username: "shenshouer",
+//		Department: "GT-基础部门",
+//	}
+//
+//	// update
+//	_, err = Update(db, ui)
+//	if err != nil{
+//		log.Fatal(err)
+//	}
+//
 //	affect, err := res.RowsAffected()
 //	if err != nil{
 //		log.Fatal(err)
 //	}
 //
 //	fmt.Println(affect)
-
-	// query
-	rows, err = db.Query("SELECT * FROM userinfo")
-	if err != nil{
-		log.Fatal(err)
-	}
-
-	for rows.Next() {
-		ui := &UserInfo{}
-		err = rows.Scan(&ui.Uid, &ui.Username, &ui.Department, &ui.Created)
-		if err != nil{
-			log.Fatal(err)
-		}
-		log.Println(ui)
-	}
+//
+//	// query
+//	rows, err = db.Query("SELECT * FROM userinfo")
+//	if err != nil{
+//		log.Fatal(err)
+//	}
+//
+//	for rows.Next() {
+//		ui := &UserInfo{}
+//		err = rows.Scan(&ui.Uid, &ui.Username, &ui.Department, &ui.Created)
+//		if err != nil{
+//			log.Fatal(err)
+//		}
+//		log.Println(ui)
+//	}
 
 	// delete
 //	stmt, err = db.Prepare("delete from userinfo where uid=?")
@@ -136,8 +131,81 @@ func main() {
 //
 //	fmt.Println(affect)
 
-	db.Close()
+	defer db.Close()
 
+}
+
+func Query(db *sql.DB, entity IEntity)([]IEntity, error){
+	structName := structs.Name(entity)
+	var registryStruct reflect.Type
+	var ok bool
+	if registryStruct, ok = typeRegistry[structName]; !ok{
+		return nil, fmt.Errorf("%s can not be registried, pleace registry before used!", structName)
+	}
+
+	querySql := fmt.Sprintf("SELECT * FROM %s", entity.TableName())
+	rows, err := db.Query(querySql)
+	if err != nil{
+		return nil, err
+	}
+
+	entities := make([]IEntity, 0)
+	columns, err := rows.Columns()
+	if err != nil{
+		return nil, err
+	}
+
+	colNum := len(columns)
+	var values = make([]interface{}, colNum)
+	for i, _ := range values {
+		var ii interface{}
+		values[i] = &ii
+	}
+
+	v := reflect.Zero(registryStruct)
+	for rows.Next() {
+//		v := reflect.New(registryStruct).Elem()
+		if tmp, ok := v.Interface().(IEntity); ok{
+			tmp = tmp.Initialize()
+			val := reflect.ValueOf(tmp).Elem()
+//			tmpType := reflect.TypeOf(val)
+			numFiled := val.NumField()
+			for i := 0; i < numFiled; i++{
+				fieldValue := val.Field(i)
+				fieldValueType := fieldValue.Type()
+				fieldType := val.Type().Field(i)
+				log.Println(fieldValueType, fieldValue, fieldType.Tag)
+			}
+
+			entities = append(entities, tmp)
+		}
+
+
+//		log.Println(v.Kind())
+//		log.Println("==>", tmp, ok)
+//		log.Println(tmp)
+//		s := structs.New(tmp)
+//		err := rows.Scan(values...)
+//		if err != nil{
+//			return nil, err
+//		}
+//		for i, fieldName := range columns{
+//			fields := s.Fields()
+//			for _, field := range fields{
+//				log.Println(fieldName,"field", field.Name(), "tag" , strings.Split(field.Tag("db"), ",")[0])
+//				if strings.Split(field.Tag("db"), ",")[0] == fieldName{
+//					log.Println(fieldName, strings.Split(field.Tag("db"), ",")[0], values[i])
+//					field.Set(values[i])
+//					log.Println("=====>> field.Value", field.Value())
+//					break
+//				}
+//			}
+//		}
+
+//		log.Println(tmp)
+//		entities = append(entities, tmp)
+	}
+	return entities, nil
 }
 
 func Update(db *sql.DB, entity IEntity) (sql.Result, error){
@@ -244,6 +312,26 @@ func Save(db *sql.DB, entity IEntity) (sql.Result, error){
 	return res, nil
 }
 
-func (ui *UserInfo) TableName() string {
+func (*UserInfo) TableName() string {
 	return "userinfo"
+}
+
+// 初始化当前指针,并将实体所有属性赋予零值
+func (ui *UserInfo) Initialize() IEntity {
+	ui = &UserInfo{}
+	log.Println(ui)
+	return ui
+}
+
+// 注册需要用到ORM的实体
+func Registry(v interface{}){
+	if _, ok := v.(IEntity); !ok{
+		panic("Registry struce must be implement the interface of IEntity")
+	}
+	structName := structs.Name(v)
+	if _, ok := typeRegistry[structName]; ok{
+		panic(fmt.Sprintf("%s has registried!"))
+	}else{
+		typeRegistry[structName] = reflect.TypeOf(v)
+	}
 }
